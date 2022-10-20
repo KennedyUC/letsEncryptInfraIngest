bd888d4a7b37fd2ad2d48e2f2aa07951fdf6b036
kennedyuc <chukwukakennedyuche@gmail.com>
2022-10-20 16:20:58 -0700

    infra-ingest init commit

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "helm_create_namespace" {
     type        = bool
-    default     = False
+    default     = false
     description = "Specify the whether namespace is created by Helm"
 }
 
 variable "enable_atomic" {
     type        = bool
-    default     = False
+    default     = false
     description = "Specify the whether atomic is enabled in Helm"
 }
 
